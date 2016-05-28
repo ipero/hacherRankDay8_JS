d0fa4f7814020b86fa6ab32185c025cd6be0b8d1
ipero <ipero@live.com>
2016-05-28 10:08:50 -0500

    Solution

---
+++ temp.js
@@ -0,0 +1,19 @@
+// Solution for challenge Day 8: Dictionaries and Maps on HackerRank.com
+function processData(input) {
+    function find(name){
+        if(name in phoneBook){
+            console.log(name + "=" + phoneBook[name]);
+        }else{
+           console.log("Not found");
+        }
+    }
+    var data = input.split('\n');
+    var n = data.shift();
+    var checkNames = data.splice(n);
+    var phoneBook={};
+    for(var i=0; i<data.length; i++){
+        var tempData = data[i].split(" ");
+        phoneBook[tempData[0]] = tempData[1];
+    }
+    checkNames.map(find);
+}
